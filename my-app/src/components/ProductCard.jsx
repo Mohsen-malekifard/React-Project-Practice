1f6aaa7d57a29c0b3db8422c824e0997173fd2db
Mohsen Malekifard <mohsen.malekifard.dev@gmail.com>
2025-08-08 13:55:30 +0000

    D

--- my-app/src/components/ProductCard.jsx
+++ my-app/src/components/ProductCard.jsx
@@ -1,12 +1,15 @@
 import data from "../data/data";
 
 export default function ProductCard({product}) {
-
+    const [isClicked , setIsClicked] = useState(false);
     return (
         <div>
-            <h1>{p.title}</h1>
-            <p>{p.desc}</p>
-            <button href = {`Product/${p.id}`} >Description</button>
+            <h1>{product.title}</h1>
+            <button onClick = {() => setIsClicked(!isClicked)} >Show Description</button>
+            {isClicked && (<p>{product.desc}</p>)}
+            <p>---------------------------</p>
+            {/* <p>{product.desc}</p> */}
+            {/* <button href = {`Product/${product.id}`} >Description</button> */}
         </div>
     )
 }
