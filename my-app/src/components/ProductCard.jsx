4b0343ab0dc38f88dd046016a4aa647d7c6a2fa1
Mohsen Malekifard <mohsen.malekifard.dev@gmail.com>
2025-08-08 14:01:29 +0000

    E

--- my-app/src/components/ProductCard.jsx
+++ my-app/src/components/ProductCard.jsx
@@ -1,12 +1,12 @@
 import data from "../data/data";
-
+import {useState} from "react"
 export default function ProductCard({product}) {
     const [isClicked , setIsClicked] = useState(false);
     return (
         <div>
             <h1>{product.title}</h1>
             <button onClick = {() => setIsClicked(!isClicked)} >Show Description</button>
-            {isClicked && (<p>{product.desc}</p>)}
+            {isClicked && <p>{product.desc}</p>}
             <p>---------------------------</p>
             {/* <p>{product.desc}</p> */}
             {/* <button href = {`Product/${product.id}`} >Description</button> */}
--- my-app/src/data/data.js
+++ my-app/src/data/data.js
@@ -1,7 +1,7 @@
 const data = [
-    {id : 1, title : "Product-one" , desc : "This is Product-one"},
-    {id : 2, title : "Product-two" , desc : "This is Product-two"},
-    {id : 3, title : "Product-three", desc : "This is Product-three"}
+    {id : 1, title : "Product-one" , desc : "Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Loerm Lorem Lorem Lorem Lorem Lorem Lorem"},
+    {id : 2, title : "Product-two" , desc : "Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Loerm Lorem Lorem Lorem Lorem Lorem Lorem"},
+    {id : 3, title : "Product-three", desc : "Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Loerm Lorem Lorem Lorem Lorem Lorem Lorem"}
 ]
 
 export default data;
